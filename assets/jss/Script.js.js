0c6946337a03f976aafa4ff07aaf5fa248ba5acd
belajarc <sintadwiarmelia11rpl2@gmail.com>
2019-05-14 13:14:30 +0700

    tugas

---
+++ assets/jss/Script.js.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+$('.page-scroll').on(click',function(e){
+	var tujuan = $(this).attr('href');
+	var elemenTujuan = $(tujuan);
+	$('html,body').animate({
+		scrollTop: elemenTujuan.offset().top-50
+	}, 900, 'swing');
+	e.preventDevault();
+});
